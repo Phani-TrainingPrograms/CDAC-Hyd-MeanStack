49ae2e8e81d6383fca07c119c6f9bd0731ab46f8
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-10-01 11:59:43 +0530

    Modified SRC folder

--- Angular18/sample-app/src/app/app.component.ts
+++ Angular18/sample-app/src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'phani',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
-  title = 'sample-app';
+  title  = "My Application Name";//data
+  name : string = "Phaniraj"
+  address : string = "Bangalore"
+  salary : number = 45000
 }
